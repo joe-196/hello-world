6d3167530febd3261e0ec40f14f7c59bb3a850da
joe-196 <95313807+joe-196@users.noreply.github.com>
2021-12-02 22:51:27 +0100

    jetzt auch mit bissl inhalt
    keine description

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ Created on Thu Dec  2 22:36:17 2021
 @author: joe
 """
 
+hello_count = 1
+print("das ist mein ", str(hello_count), "Hello Word mit github :-)")
+hello_count += 1
+print(str(hello_count))
